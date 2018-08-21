33ceba710d7a9a627919361b57c37a3e300680ab
michael tamsil <michaeltamsil@gmail.com>
2018-08-21 15:23:52 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,16 @@
+const apikey = 'AGheDDdseSWk73IRnYrMAz'; // get your API key from filestask
+const client = filestack.init(apikey);
+
+document.querySelector('input').addEventListener('change', (event) => {
+  debugger;
+  const files = event.target.files;
+  const file = files.item(0);
+
+  client.upload(file)
+    .then(res => {
+      console.log('success: ', res)
+    })
+    .catch(err => {
+      console.log(err)
+    });
+  });
